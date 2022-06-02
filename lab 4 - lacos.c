874a4b0dffbc94f1a0a8f8cb97b0449de7098e27
joaomarcelolisboa <106291379+joaomarcelolisboa@users.noreply.github.com>
2022-06-01 23:11:57 -0300

    Add files via upload

---
+++ lab 4 - lacos.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ex 01
+int main()
+{
+    int i, n=0;
+
+    for(i = 0; i < 5; i++){
+        n += 3;
+
+    printf("%d\n", n);
+    }
+
+
+    return 0;
+}
+*/
+/* ex 02
+int main()
+{
+    int i, j = 1, k = 0, n = 0, o = 0, p = 0;
+
+    for(i = 1; i <= 100; i++){
+
+        printf("%d\n", n += 1);
+    }
+    while(j<= 100){
+        printf("%d\n", o += 1);
+
+        j++;
+    }
+    do{
+        printf("%d\n", p += 1);
+        k++;
+
+    }while(k<100);
+
+    return 0;
+}
+*/
+/* ex 03
+int main()
+{
+    int i = 0, n=10;
+
+    while(i <= 10){
+
+        printf("%d\n", n --);
+    i++;
+}
+    printf("FIM!");
+
+return 0;
+}
+*/
+/* ex 04
+int main()
+{
+    int i = 0, n = 0;
+
+    for(i = 0; i <= 100; i++){
+        printf("%d\n", n);
+        n += 1000;
+    }
+
+return 0;
+}
+*/
+/* ex 05
+int main()
+{
+  int i, soma;
+  soma = 0;
+  int num;
+
+  for(i= 0; i<10; i++) {
+    printf("Digite um numero: ", i+1);
+    scanf("%d", &num);
+    soma +=num;
+  }
+  printf("\nSoma = %d : ", soma);
+
+   return 0;
+}
+*/
+/* ex 07
+int main()
+{
+  int i, media;
+  media = 0;
+  int num;
+
+  for(i= 0; i<10; i++) {
+    printf("Digite um numero: ", i+1);
+    scanf("%d", &num);
+    media +=num;
+  }
+  printf("\nMedia = %d : ", media/10);
+
+   return 0;
+}
+*/
+
